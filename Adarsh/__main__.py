d977db9e3ae2ad35b2525ce8e072707d45e4d114
johnsonveigas <70007806+johnsonveigas@users.noreply.github.com>
2023-09-03 14:41:16 +0530

    Update __main__.py

--- Adarsh/__main__.py
+++ Adarsh/__main__.py
@@ -7,7 +7,7 @@ import logging
 import importlib
 from pathlib import Path
 from pyrogram import idle
-from bot import StreamBot
+from .bot import StreamBot
 from vars import Var
 from aiohttp import web
 #from .server import web_server
